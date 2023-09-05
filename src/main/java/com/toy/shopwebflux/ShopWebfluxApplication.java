e07a53586fe4915a3c002f39e01f375468a356ab
inkyu192 <inkyu192@gmail.com>
2023-09-05 23:46:21 +0900

    postgres 추가

--- src/main/java/com/toy/shopwebflux/ShopWebfluxApplication.java
+++ src/main/java/com/toy/shopwebflux/ShopWebfluxApplication.java
@@ -2,7 +2,11 @@ package com.toy.shopwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+@EnableR2dbcAuditing
+@EnableR2dbcRepositories
 @SpringBootApplication
 public class ShopWebfluxApplication {
 
