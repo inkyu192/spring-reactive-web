2733fb805f7302f00bee2bda5ca3b782834b9f2d
inkyu192 <inkyu192@gmail.com>
2023-09-16 23:41:05 +0900

    수정

--- src/main/java/com/toy/shopwebflux/common/ControllerAdvice.java
+++ src/main/java/com/toy/shopwebflux/common/ControllerAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reactor.core.publisher.Mono;
 
+import static com.toy.shopwebflux.common.ApiResponseCode.*;
+
 
 @Slf4j
 @RestControllerAdvice
@@ -14,5 +16,10 @@ public class ControllerAdvice {
     public Mono<ApiResponse<Void>> commonExceptionHandler(CommonException e) {
         ApiResponseCode apiResponseCode = e.getApiResponseCode();
         return Mono.just(new ApiResponse<>(apiResponseCode.getCode(), apiResponseCode.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Mono<ApiResponse<Void>> exceptionHandler(Exception e) {
+        return Mono.just(new ApiResponse<>(INTERNAL_SERVER_ERROR.getCode(), INTERNAL_SERVER_ERROR.getMessage()));
     }
 }
