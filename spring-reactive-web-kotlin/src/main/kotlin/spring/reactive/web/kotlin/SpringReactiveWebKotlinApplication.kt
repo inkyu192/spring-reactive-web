22be796da57f5fee047a19c2c61e1efa52c381a1
inkyu192 <inkyu192@gmail.com>
2024-01-07 11:42:51 +0900

    수정

---
+++ spring-reactive-web-kotlin/src/main/kotlin/spring/reactive/web/kotlin/SpringReactiveWebKotlinApplication.kt
@@ -0,0 +1,11 @@
+package spring.reactive.web.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactiveWebKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringReactiveWebKotlinApplication>(*args)
+}
---
+++ spring-reactive-web-kotlin/src/test/kotlin/spring/reactive/web/kotlin/SpringReactiveWebKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package spring.reactive.web.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringReactiveWebKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
