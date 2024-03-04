7a8b3c90ff22186e0c85817d34e0a10f88f097b1
inkyu192 <inkyu192@gmail.com>
2024-03-04 10:29:48 +0900

    수정

--- spring-reactive-web-java/src/main/java/spring/reactive/web/java/controller/CustomerController.java
+++ spring-reactive-web-java/src/main/java/spring/reactive/web/java/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package spring.reactive.web.java.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
-import reactor.util.function.Tuple2;
 import spring.reactive.web.java.domain.Customer;
 import spring.reactive.web.java.repository.CustomerRepository;
-
-import java.time.Duration;
 
 @RestController
 public class CustomerController {
@@ -32,13 +28,6 @@ public class CustomerController {
                 .filter(customer -> customer.getFirstName().equals(firstname))
                 .map(customer -> ServerSentEvent.builder(customer).build())
                 .doOnCancel(() -> sink.asFlux().blockLast());
-    }
-
-    @GetMapping(value = "/customer/sse2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Customer> test() {
-        return Flux
-                .zip(Flux.interval(Duration.ofSeconds(1)), customerRepository.findAll().repeat())
-                .map(Tuple2::getT2);
     }
 
     @PostMapping("/customer")
