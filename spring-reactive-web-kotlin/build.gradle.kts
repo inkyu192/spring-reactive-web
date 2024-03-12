20e0bb5726d414639e0dd4a450cef8b908d66310
inkyu192 <inkyu192@gmail.com>
2024-03-12 16:16:31 +0900

    수정

--- spring-reactive-web-kotlin/build.gradle.kts
+++ spring-reactive-web-kotlin/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
